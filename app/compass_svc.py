54a96f0d5718d83fca6d465e7e56d3bbbb69c3e4
wbooth <18699738+wbooth@users.noreply.github.com>
2019-12-19 12:39:10 -0500

    feedback

--- app/compass_svc.py
+++ app/compass_svc.py
@@ -1,5 +1,3 @@
-import json
-
 from aiohttp import web
 from aiohttp_jinja2 import template
 
@@ -12,42 +10,42 @@ class CompassService:
     @template('compass.html')
     async def splash(self, request):
         await self.services.get('auth_svc').check_permissions(request)
-        return dict(test='test')
+        return dict()
 
     @staticmethod
-    def get_layer_boilerplate(name, description):
-        return {
-            'version': '2.2',
-            'name': name,
-            'description': description,
-            'domain': 'mitre-enterprise',
-            'techniques': [],
-            'legendItems': [],
-            'showTacticRowBackground': True,
-            'tacticRowBackground': '#205b8f',
-            'selectTechniquesAcrossTactics': True,
-            'gradient': {
-                'colors': [
+    def _get_layer_boilerplate(name, description):
+        return dict(
+            version='2.2',
+            name=name,
+            description=description,
+            domain='mitre-enterprise',
+            techniques=[],
+            legendItems=[],
+            showTacticRowBackground=True,
+            tacticRowBackground='#205b8f',
+            selectTechniquesAcrossTactics=True,
+            gradient=dict(
+                colors=[
                     '#ffffff',
                     '#66ff66'
                 ],
-                'minValue': 0,
-                'maxValue': 1
-            }
-        }
+                minValue=0,
+                maxValue=1
+            )
+        )
 
     async def generate_layer(self, request):
+        await self.services.get('auth_svc').check_permissions(request)
         abilities = await self.services.get('data_svc').locate('abilities')
-
-        layer = self.get_layer_boilerplate(name='All-Abilities', description='full set of techniques available')
+        layer = self._get_layer_boilerplate(name='All-Abilities', description='full set of techniques available')
         for ability in abilities:
-            technique = {
-                'techniqueID': ability.technique_id,
-                'score': 1,
-                'color': '',
-                'comment': '',
-                'enabled': True
-            }
+            technique = dict(
+                techniqueID=ability.technique_id,
+                score=1,
+                color='',
+                comment='',
+                enabled=True
+            )
             layer['techniques'].append(technique)
 
         return web.json_response(layer)
