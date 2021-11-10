fb88e0274a79ab87ad0965595aaea645a5ce0b62
Anna Lin <alin@mitre.org>
2021-11-10 08:41:54 -0500

    Compass UI update (#43)
    * Updating compass UI
    
    * Updated buttons look and feel
    
    * Moved instructions in modal to buttons as tooltips
    
    * Updated instructive text as phases is not currently in CALDERA
    
    * Compass can now upload adversary layers, and show unmatched techniques

--- hook.py
+++ hook.py
@@ -8,7 +8,6 @@ address = '/plugin/compass/gui'
 async def enable(services):
     app = services.get('app_svc').application
     compass_svc = CompassService(services)
-    app.router.add_static('/compass', 'plugins/compass/static/', append_version=True)
     app.router.add_route('POST', '/plugin/compass/layer', compass_svc.generate_layer)
     app.router.add_route('POST', '/plugin/compass/adversary', compass_svc.create_adversary_from_layer)
     app.router.add_route('GET', '/plugin/compass/gui', compass_svc.splash)
